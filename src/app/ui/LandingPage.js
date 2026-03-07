12ad850f9bf9157a7ae8fb3dca5e3c70decb277a
J0hnux <jc24.doliente@gmail.com>
2026-03-07 23:43:05 +0800

    Navigation initial design

--- src/app/ui/LandingPage.js
+++ src/app/ui/LandingPage.js
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import {
+  Calendar,
+  MapPin,
+  ChevronRight,
+  Search,
+  User,
+  Menu,
+  X,
+} from "lucide-react";
+import { cars } from "../data/cars";
+import { features } from "../data/cars";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-slate-50 font-sans">
+      <nav
+        className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+          isScrolled ? "glass-effect shadow-lg" : "bg-transparent"
+        }`}
+      ></nav>
+    </div>
+  );
 };
 
 export default LandingPage;
